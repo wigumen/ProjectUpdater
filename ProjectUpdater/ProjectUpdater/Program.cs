b2626e840aee0f56057f5a12eb3d637a2c517348
Martin <martin.wigum@gmail.com>
2014-12-16 18:24:33 +0100

    Started boilerplate work

--- ProjectUpdater/ProjectUpdater/Program.cs
+++ ProjectUpdater/ProjectUpdater/Program.cs
@@ -8,8 +8,20 @@ namespace ProjectUpdater
 {
     class Program
     {
+        /// <summary>
+        /// Main class for SlickUpdater
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            //Parse all arguments
+            foreach (string arg in args)
+            {
+                if(arg == "update")
+                {
+
+                }
+            }
         }
     }
 }
---
+++ ProjectUpdater/ProjectUpdater/Updater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectUpdater
+{
+    class Updater
+    {
+        /// <summary>
+        /// The actual updater class, this downloads everything from internet repos to your local client
+        /// </summary>
+        /// <param name="URL">The web link to the root of the repository</param>
+        /// <param name="Path">Path to where to download repository</param>
+        public void UpdateRepo(string URL, string Path)
+        {
+
+        }
+    }
+}
